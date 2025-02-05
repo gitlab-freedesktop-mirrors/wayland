7c2ffb0d712ca1aafbfa94c71f80226490bcbd28
David Redondo <kde@david-redondo.de>
2025-02-05 09:21:43 +0100

    Make wayland-util.h -Wundef safe when compiled by a C++ compiler
    Fixes #522
    Signed-off-by: David Redondo <kde@david-redondo.de>

--- src/wayland-util.h
+++ src/wayland-util.h
@@ -48,7 +48,7 @@ extern "C" {
 #endif
 
 /** Deprecated attribute */
-#if __STDC_VERSION__ >= 202311L
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
 #define WL_DEPRECATED [[deprecated]]
 #elif defined(__GNUC__) && __GNUC__ >= 4
 #define WL_DEPRECATED __attribute__ ((deprecated))
@@ -70,7 +70,7 @@ extern "C" {
 #define WL_PRINTF(x, y)
 #endif
 
-#if __STDC_VERSION__ >= 202311L
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
 #define WL_TYPEOF(expr) typeof(expr)
 #else
 #define WL_TYPEOF(expr) __typeof__(expr)
